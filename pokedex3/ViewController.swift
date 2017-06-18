0e82a4436a5d3fead191b27571a0f95897525881
Varol AKSOY <mac@Varols-Mac-Mini.local>
2017-06-18 21:45:52 +0300

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Mac on 18.06.2017.
+//  Copyright © 2017 Varol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
